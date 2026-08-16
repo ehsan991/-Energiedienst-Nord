be738d87c5d1e8337e61ac1c69a1e73175090cde
Ehsan Parastegari <87284366+ehsan991@users.noreply.github.com>
2026-08-16 22:06:16 +0200

    Remove Payload admin from frontend preview

--- src/app/(payload)/admin/[[...segments]]/not-found.tsx
+++
@@ -1,4 +0,0 @@
-import config from '@payload-config'
-import { NotFoundPage } from '@payloadcms/next/views'
-import { importMap } from '../importMap.js'
-export default function NotFound() { return NotFoundPage({ config, importMap }) }
